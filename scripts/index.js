aed4cbf1292b7d67866e166ecf27a715b0166e9d
Tatyana Koba <misskotanyaz@gmail.com>
2023-05-01 11:26:01 +0300

    changed files

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,23 @@
 const profileEditButton = document.querySelector('.profile__edit-button');
 const popupEdit = document.querySelector('.popup');
 const buttonEditClose = document.querySelector('.popup__button-close');
-
-const togglePopupState = (popupToToggle) => popupToToggle.classList.toggle('popup_opened');
-
-profileEditButton.addEventListener('click', () => togglePopupState(popupEdit));
-
-buttonEditClose.addEventListener('click', () => togglePopupState(popupEdit));
-
-popupEdit.addEventListener('click', (evt) => {
-    if (evt.target === evt.currentTarget) {
-        togglePopupState(popupEdit);
-    }
-})
-
 const formElement = document.querySelector('.popup__form');
-const nameInput = formElement.querySelector('.popup__input_name');
-const jobInput = formElement.querySelector('.popup__input_about');
+const nameInput = formElement.querySelector('.popup__input_type_name');
+const jobInput = formElement.querySelector('.popup__input_type_about');
 let profileName = document.querySelector('.profile__name');
 let profileAbout = document.querySelector('.profile__description');
+
+profileEditButton.addEventListener ('click', () => {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileAbout.textContent;
+    popupEdit.classList.add('popup_opened');
+});
+
+function closePopup() {
+    popupEdit.classList.remove('popup_opened');
+}
+
+buttonEditClose.addEventListener ('click', closePopup);
 
 function handleFormSubmit (evt) {
     evt.preventDefault(); 
@@ -26,7 +25,7 @@ function handleFormSubmit (evt) {
     profileName.textContent = nameInput.value;
     profileAbout.textContent = jobInput.value;
 
-    togglePopupState(popupEdit);
+    closePopup();
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
